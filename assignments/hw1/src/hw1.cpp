d3b5572a9021e57470edfb5e647daaeb9261ab34
Jose Carlos Martinez Garcia-Vaso <carlosgvaso@gmail.com>
2020-06-15 13:55:35 -0500

    Added Utkash's solution to problem 5 of hw1

--- assignments/hw1/src/hw1.cpp
+++ assignments/hw1/src/hw1.cpp
@@ -1,12 +1,51 @@
 #include <iostream>
 #include <omp.h>
+#include <cmath>
 #include "hw1.h"
 
 double euclidean_length(std::vector<double> vector) {
   // Your code goes here.
-  return 0;
+  int n = vector.size();
+
+  if(n <= 0){
+    return 0;
+  }
+
+  int i;
+  double sum = 0.0;
+  double result = 0.0;
+
+
+#pragma opm parallel for reduction(+:sum) // shared(sum, vector) private(i)
+  for(i=0; i<n; i++){
+    //std::cout << "Number of threads: " << omp_get_thread_num() << std::endl;
+    sum += vector[i]*vector[i];
+  }
+
+  result=sqrt(sum);
+
+  return result;
 }
 std::vector<int64_t> discard_duplicates(std::vector<int64_t> sorted_vector) {
   // Your code goes here
-  return std::vector<int64_t>();
+  int n = sorted_vector.size();
+
+  if(n <= 1){
+    return sorted_vector;
+  }
+
+  // Declare vector to return with no duplicates
+  std::vector<int64_t> no_dup_vector;
+/* WIP
+  // Get size of input vector
+  int64_t n = sorted_vector.size();
+
+  for (int h=1; h<=ceil(log2((double)n)); h++) {
+#pragma omp parallel for shared(sorted_vector, no_dup_vector)
+    for (int i=1; i<=ceil(n/h); i+=2) {
+      //
+    }
+  }
+*/
+  return no_dup_vector;
 }
