e3f21bd1405f8667aa85c4a88cdee6ece6284561
Jose Carlos Martinez Garcia-Vaso <carlosgvaso@gmail.com>
2020-06-16 15:02:27 -0500

    Added missing code for discard_duplicates(), added debugging output and controls, and added testing cases for discard_duplicates()

--- assignments/hw1/src/hw1.cpp
+++ assignments/hw1/src/hw1.cpp
@@ -4,6 +4,23 @@
 #include <cmath>
 #include "hw1.h"
 
+#define DEBUG 0 // Enable (1) / disable (0) debugging output
+
+/*
+ * Allows us to easily print a vector in the format [#, #, #]
+ */
+template<typename T>
+std::ostream& operator<<(std::ostream& s, const std::vector<T>& v) 
+{
+    s.put('[');
+    char comma[3] = {'\0', ' ', '\0'};
+    for (const auto& e : v) {
+        s << comma << e;
+        comma[0] = ',';
+    }
+    return s << ']';
+}
+
 double euclidean_length(std::vector<double> vector) {
   // Your code goes here.
   int n = vector.size();
@@ -17,12 +34,14 @@ double euclidean_length(std::vector<double> vector) {
   double sum = 0.0;
   double result = 0.0;
 
-
 #pragma opm parallel for default(shared) private(i) schedule(static,chunk) reduction(+:sum)
   for(i=0; i<n; i++){
     sum += vector[i]*vector[i];
 
-    //std::cout << "Number of threads: " << omp_get_thread_num() << std::endl;
+    // Debugging
+#if DEBUG == 1
+    std::cout << "Number of threads: " << omp_get_thread_num() << std::endl;
+#endif
   }
 
   result=sqrt(sum);
@@ -31,24 +50,124 @@ double euclidean_length(std::vector<double> vector) {
 }
 std::vector<int64_t> discard_duplicates(std::vector<int64_t> sorted_vector) {
   // Your code goes here
-  int n = sorted_vector.size();
 
+  // Debugging
+#if DEBUG == 1
+  std::cout << "A: " << sorted_vector << std::endl;
+#endif
+
+  // Get size of input vector also known as vector A
+  int64_t n = sorted_vector.size();
+
+  // If the vector has size of 1 or less, it can't have any repeated values
   if(n <= 1){
     return sorted_vector;
   }
 
-  // Declare vector to return with no duplicates
-  std::vector<int64_t> no_dup_vector;
-/* WIP
-  // Get size of input vector
-  int64_t n = sorted_vector.size();
+  // Declare auxiliary vectors C and D
+  std::vector<int64_t> C(n), D(n);
+  int64_t i, i_step, i_step_l, h, h_max, l;
 
-  for (int h=1; h<=ceil(log2((double)n)); h++) {
-#pragma omp parallel for shared(sorted_vector, no_dup_vector)
-    for (int i=1; i<=ceil(n/h); i+=2) {
-      //
+  /*
+   * Create an array C such that C[i] = 1, if A[i] != A[i−1],
+   * and 0 otherwise (A is the input vector sorted_vector)
+   */
+#pragma omp parallel for default(shared) private(i)
+  for (i=0; i<n; i++) {
+    if (i == 0) { // First entry is always 1
+      C[i] = 1;
+    } else if (sorted_vector[i] != sorted_vector[i-1]) {
+      C[i] = 1;
+    } else {
+      C[i] = 0;
     }
   }
-*/
-  return no_dup_vector;
+
+  // Debugging
+#if DEBUG == 1
+  std::cout << "C: " << C << std::endl;
+#endif
+
+  // Create array D as the prefix sum of C
+  // Set up auxiliary vector D
+#pragma omp parallel for default(shared) private(i)
+  for (i=0; i<n; i++) {
+    D[i] = C[i];
+  }
+
+  // Debugging
+#if DEBUG == 1
+  std::cout << "D1: " << D << std::endl;
+#endif
+
+  // Upward sweep (reduce operation)
+  h_max = ((int64_t)log2((double)n)) - 1;
+  for (h=0; h<=h_max; h++) {
+    i_step = (int64_t)pow(2.0, (double)(h+1));
+    i_step_l = (int64_t)pow(2.0, (double)h);
+#pragma omp parallel for default(shared) private(i)
+    for (i=0; i<n; i+=i_step) {
+      D[i + i_step - 1] = D[i + i_step_l - 1] + D[i + i_step - 1];
+    }
+  }
+
+  // Debugging
+#if DEBUG == 1
+  std::cout << "D2: " << D << std::endl;
+#endif
+
+  // Downward sweep
+  D[n-1] = 0;
+  for (h=h_max; h>=0; h--) {
+    i_step = (int64_t)pow(2.0, (double)(h+1));
+    i_step_l = (int64_t)pow(2.0, (double)h);
+#pragma omp parallel for default(shared) private(i, l)
+    for (i=0; i<n; i+=i_step) {
+      l = D[i + i_step_l - 1];
+      D[i + i_step_l - 1] = D[i + i_step - 1];
+      D[i + i_step - 1] = l + D[i + i_step - 1];
+    }
+  }
+
+  // Debugging
+#if DEBUG == 1
+  std::cout << "D3: " << D << std::endl;
+#endif
+
+  /*
+   * Format the D vector so that the values getting written concurrently are the
+   * the same values (common concurrent write), and any collissions caused by
+   * duplicated values are solved that way. This can be done by transforming the
+   * exclusive scan to inclusive scan, and subtracting all entries by 1 so that
+   * the indexes start at 0.
+   */
+#pragma omp parallel for default(shared) private(i)
+  for (i=0; i<n; i++) {
+    D[i] = D[i] + C[i] - 1;
+  }
+
+  // Debugging
+#if DEBUG == 1
+  std::cout << "D4: " << D << std::endl;
+#endif
+
+  /*
+   * Calculate the solution array B from D, such that if D[i] = k, then the
+   * number of distinct elements in A which are smaller than or equal A[i] is k.
+   * Therefore, A[i] should go in the kth position in B. Notice, B is of size
+   * D[n-1] + 1 (last entry of vector D plus the 1 we removed earlier), since
+   * we used an inclusive scan.
+   */
+  std::vector<int64_t> B(D[n-1] + 1);
+
+#pragma omp parallel for default(shared) private(i)
+  for (i=0; i<n; i++) {
+    if (i == 0) { // First entry is always added
+      B[D[i]] = sorted_vector[i];
+    } else if (C[i] == 1) { //(sorted_vector[i] != sorted_vector[i-1]) {
+      B[D[i]] = sorted_vector[i];
+    }
+  }
+
+  return B;
 }
--- assignments/hw1/src/main.cpp
+++ assignments/hw1/src/main.cpp
@@ -2,12 +2,15 @@
 #include <vector>
 #include "hw1.h"
 
+#define DEBUG 0 // Enable (1) / disable (0) debugging output
+
 int main() {
   // Example execution for problem 5
   std::vector<double> vector = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   std::cout << "Euclidean distance: " << euclidean_length(vector) << std::endl; // This should print 19.62...
 
-  // Exacmple execution for problem 6
+  // Example execution for problem 6
+#if DEBUG == 0
   std::vector<int64_t> sorted_with_dups = {1, 2, 2, 4, 5, 5, 6, 6};
   std::vector<int64_t> result = discard_duplicates(sorted_with_dups);
 
@@ -16,5 +19,52 @@ int main() {
   for (auto i = result.begin(); i != result.end(); ++i)
     std::cout << *i << ' ';
   std::cout << std::endl;
+
+#else
+  std::vector<std::vector<int64_t>> vectors_with_dups {
+    {},
+    {5},
+    {2, 5},
+    {5, 5},
+    {1, 1, 2, 3},
+    {1, 2, 3, 3},
+    {1, 2, 2, 3},
+    {1, 2, 3, 4},
+    {1, 2, 2, 4, 5, 5, 6, 6},
+    {1, 1, 1, 2, 2, 4, 5, 5, 6, 6, 6, 6, 7, 9, 10, 16}
+  };
+
+  std::vector<std::vector<int64_t>> vectors_without_dups {
+    {},
+    {5},
+    {2, 5},
+    {5},
+    {1, 2, 3},
+    {1, 2, 3},
+    {1, 2, 3},
+    {1, 2, 3, 4},
+    {1, 2, 4, 5, 6},
+    {1, 2, 4, 5, 6, 7, 9, 10, 16}
+  };
+
+  for (int j=0; j<vectors_with_dups.size(); j++) {
+    for (int k=0; k<5; k++) {
+      std::vector<int64_t> result = discard_duplicates(vectors_with_dups[j]);
+
+      //  This will print the result
+      std::cout << "Sorted vector: ";
+      for (auto i = result.begin(); i != result.end(); ++i)
+        std::cout << *i << ' ';
+      std::cout << std::endl;
+
+      if (result != vectors_without_dups[j]) {
+        std::cout << "Wrong answer!" << std::endl;
+      } else {
+        std::cout << "Right answer!" << std::endl;
+      }
+    }
+  }
+#endif
+
   return 0;
 }
